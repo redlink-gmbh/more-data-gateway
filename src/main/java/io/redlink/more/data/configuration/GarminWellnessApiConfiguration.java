549968368a475fcb820e4ef405ba43ea8585b89a
Jan Cortiel <jan.cortiel@redlink.at>
2025-11-05 10:11:58 +0100

    #95: PR changes

--- src/main/java/io/redlink/more/data/configuration/GarminWellnessApiConfiguration.java
+++ src/main/java/io/redlink/more/data/configuration/GarminWellnessApiConfiguration.java
@@ -3,22 +3,21 @@ package io.redlink.more.data.configuration;
 import io.redlink.more.data.garmin.wellness.ApiClient;
 import io.redlink.more.data.garmin.wellness.client.UserApiApi;
 import io.redlink.more.data.garmin.wellness.client.UserControllerApi;
-import io.redlink.more.data.model.garmin.UserAccessTokenWithData;
-import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Configuration
 public class GarminWellnessApiConfiguration {
-    public UserApiApi getUserApi(UserAccessTokenWithData userAccessTokenWithData) {
-        ApiClient apiClient = new ApiClient();
-        apiClient.addDefaultHeader(HttpHeaders.AUTHORIZATION, StringUtils.capitalize(userAccessTokenWithData.accessToken().tokenType()) + " " + userAccessTokenWithData.accessToken().accessToken());
-        return new UserApiApi(apiClient);
+    @Bean
+    @SessionScope
+    public UserApiApi userApiApi() {
+        return new UserApiApi(new ApiClient());
     }
 
-    public UserControllerApi getUserControllerApi(UserAccessTokenWithData userAccessTokenWithData) {
-        ApiClient apiClient = new ApiClient();
-        apiClient.addDefaultHeader(HttpHeaders.AUTHORIZATION, StringUtils.capitalize(userAccessTokenWithData.accessToken().tokenType()) + " " + userAccessTokenWithData.accessToken().accessToken());
-        return new UserControllerApi(apiClient);
+    @Bean
+    @SessionScope
+    public UserControllerApi userControllerApi() {
+        return new UserControllerApi(new ApiClient());
     }
 }
