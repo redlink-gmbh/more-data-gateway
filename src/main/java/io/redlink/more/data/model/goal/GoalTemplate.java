e8b1ad42af8ab81ff385b2fda4ae609e41aeac49
Rupert Westenthaler <rupert.westenthaler@gmail.com>
2026-06-03 11:43:14 +0200

    #388: The GoalController was not supporting the new `goal-<id>` ids o… (#45)
    * #388: The GoalController was not supporting the new `goal-<id>` ids of Goals. Switched over to the new parsing method for external IDs
    
    * #388: Fixed an issue where the goalconfig would have no values set and fixed some tests
    
    ---------
    
    Co-authored-by: Rupert Westenthaler <rupert.westenthaler@redlink.at>
    Co-authored-by: Jan Cortiel <jan.cortiel@redlink.at>

--- src/main/java/io/redlink/more/data/model/goal/GoalTemplate.java
+++ src/main/java/io/redlink/more/data/model/goal/GoalTemplate.java
@@ -10,8 +10,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GoalTemplate {
-    public static final String GAOL_TEMPLATE_ID_PREFIX = "goaltemplate-";
-    private static final Pattern PATTERN_GOAL_TEMPLATE_ID = Pattern.compile("^" + GAOL_TEMPLATE_ID_PREFIX + "(\\d+)$");
+    public static final String GOAL_TEMPLATE_ID_PREFIX = "goaltemplate-";
+    private static final Pattern PATTERN_GOAL_TEMPLATE_ID = Pattern.compile("^" + GOAL_TEMPLATE_ID_PREFIX + "(\\d+)$");
 
     private Long studyId;
     private Integer templateId;
@@ -29,56 +29,131 @@ public class GoalTemplate {
     private Set<Integer> adherenceCheckIds = new HashSet<>();
 
     // Getters & Setters (fluent)
-    public Long getStudyId() { return studyId; }
-    public GoalTemplate setStudyId(Long studyId) { this.studyId = studyId; return this; }
-
-    public Integer getTemplateId() { return templateId; }
-    public GoalTemplate setTemplateId(Integer templateId) { this.templateId = templateId; return this; }
-
-    public String getExternalTemplateId() {
-        return GAOL_TEMPLATE_ID_PREFIX + templateId;
+    public Long getStudyId() {
+        return studyId;
     }
 
-    public String getTitle() { return title; }
-    public GoalTemplate setTitle(String title) { this.title = title; return this; }
+    public GoalTemplate setStudyId(Long studyId) {
+        this.studyId = studyId;
+        return this;
+    }
 
-    public String getParticipantTitle() { return participantTitle; }
-    public GoalTemplate setParticipantTitle(String participantTitle) { this.participantTitle = participantTitle; return this; }
+    public Integer getTemplateId() {
+        return templateId;
+    }
 
-    public String getParticipantInfo() { return participantInfo; }
-    public GoalTemplate setParticipantInfo(String participantInfo) { this.participantInfo = participantInfo; return this; }
+    public GoalTemplate setTemplateId(Integer templateId) {
+        this.templateId = templateId;
+        return this;
+    }
 
-    public String getType() { return type; }
-    public GoalTemplate setType(String type) { this.type = type; return this; }
+    public String getExternalTemplateId() {
+        return GOAL_TEMPLATE_ID_PREFIX + templateId;
+    }
 
-    public String getKind() { return kind; }
-    public GoalTemplate setKind(String kind) { this.kind = kind; return this; }
+    public String getTitle() {
+        return title;
+    }
 
-    public Integer getStudyGroupId() { return studyGroupId; }
-    public GoalTemplate setStudyGroupId(Integer studyGroupId) { this.studyGroupId = studyGroupId; return this; }
+    public GoalTemplate setTitle(String title) {
+        this.title = title;
+        return this;
+    }
 
-    public Object getProperties() { return properties; }
-    public GoalTemplate setProperties(Object properties) { this.properties = properties; return this; }
+    public String getParticipantTitle() {
+        return participantTitle;
+    }
 
-    public Instant getCreated() { return created; }
-    public GoalTemplate setCreated(Instant created) { this.created = created; return this; }
+    public GoalTemplate setParticipantTitle(String participantTitle) {
+        this.participantTitle = participantTitle;
+        return this;
+    }
 
-    public Instant getModified() { return modified; }
-    public GoalTemplate setModified(Instant modified) { this.modified = modified; return this; }
+    public String getParticipantInfo() {
+        return participantInfo;
+    }
 
-    public Set<Integer> getObservationGroupIds() { return observationGroupIds; }
+    public GoalTemplate setParticipantInfo(String participantInfo) {
+        this.participantInfo = participantInfo;
+        return this;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public GoalTemplate setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public GoalTemplate setKind(String kind) {
+        this.kind = kind;
+        return this;
+    }
+
+    public Integer getStudyGroupId() {
+        return studyGroupId;
+    }
+
+    public GoalTemplate setStudyGroupId(Integer studyGroupId) {
+        this.studyGroupId = studyGroupId;
+        return this;
+    }
+
+    public Object getProperties() {
+        return properties;
+    }
+
+    public GoalTemplate setProperties(Object properties) {
+        this.properties = properties;
+        return this;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+
+    public GoalTemplate setCreated(Instant created) {
+        this.created = created;
+        return this;
+    }
+
+    public Instant getModified() {
+        return modified;
+    }
+
+    public GoalTemplate setModified(Instant modified) {
+        this.modified = modified;
+        return this;
+    }
+
+    public Set<Integer> getObservationGroupIds() {
+        return observationGroupIds;
+    }
+
     public GoalTemplate setObservationGroupIds(Set<Integer> observationGroupIds) {
         this.observationGroupIds = observationGroupIds != null ? observationGroupIds : new HashSet<>();
         return this;
     }
 
-    public Set<String> getTopicKeys() { return topicKeys; }
+    public Set<String> getTopicKeys() {
+        return topicKeys;
+    }
+
     public GoalTemplate setTopicKeys(Set<String> topicKeys) {
         this.topicKeys = topicKeys != null ? topicKeys : new HashSet<>();
         return this;
     }
 
-    public Set<Integer> getAdherenceCheckIds() { return adherenceCheckIds; }
+    public Set<Integer> getAdherenceCheckIds() {
+        return adherenceCheckIds;
+    }
+
     public GoalTemplate setAdherenceCheckIds(Set<Integer> adherenceCheckIds) {
         this.adherenceCheckIds = adherenceCheckIds != null ? adherenceCheckIds : new HashSet<>();
         return this;
@@ -86,7 +161,7 @@ public class GoalTemplate {
 
     public static Integer parseExternalTemplateId(String templateId) throws ConflictException {
         Matcher m = PATTERN_GOAL_TEMPLATE_ID.matcher(Objects.requireNonNull(templateId));
-        if(!m.find()){
+        if (!m.find()) {
             throw new ConflictException("Invalid template ID: " + templateId);
         }
         try {
